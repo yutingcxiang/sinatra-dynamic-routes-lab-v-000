0b64b8ed062564c5a326aef9a793457dec437dd7
Christine Xiang <yutingcxiang@gmail.com>
2018-08-18 22:31:16 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -24,7 +24,7 @@ class App < Sinatra::Base
     @word3 = params[:word3]
     @word4 = params[:word4]
     @word5 = params[:word5]
-     "#{@word1} #{@word2}"               
+     "#{@word1} #{@word2} #{@word3} #{@word4} #{@word5}"               
   end
   
   get '/:operation/:number1/:number2' do
