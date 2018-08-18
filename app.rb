2ff828daab825f3de4be6393c6f902ebe52c0e5e
Christine Xiang <yutingcxiang@gmail.com>
2018-08-18 22:23:37 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,5 +2,24 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
-
+  get '/reversename/:name' do
+    @name = params[:name]
+    "#{@name.reverse!}"
+  end
+  
+  get '/square/:number' do
+    @number = params[:number].to_i
+    "#{@number**2}"
+  end
+  
+  get '/say/:number/:phrase'
+    
+  end
+  
+  get '/say/:word1/:word2/:word3/:word4/:word5'
+  end
+  
+  get '/:operation/:number1/:number2'
+    @operation
+  end
 end
