7ac91ed1e9cba10b1e471f165ce5aa4483127cce
Christine Xiang <yutingcxiang@gmail.com>
2018-08-18 22:29:50 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -12,13 +12,15 @@ class App < Sinatra::Base
     "#{@number**2}"
   end
   
-  get '/say/:number/:phrase'
-    
+  get '/say/:number/:phrase' do
+    @number = params[:number].to_i
+    @phrase = params[:phrase]
+    "#{@phrase * @number}"
   end
   
-  get '/say/:word1/:word2/:word3/:word4/:word5'
+  get '/say/:word1/:word2/:word3/:word4/:word5' do
   end
   
-  get '/:operation/:number1/:number2'
+  get '/:operation/:number1/:number2' do
   end
 end
