1a21d12fb4a83aed4781b9737bdda12eefa15686
Christine Xiang <yutingcxiang@gmail.com>
2018-08-18 22:33:24 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -28,10 +28,10 @@ class App < Sinatra::Base
   end
   
   get '/:operation/:number1/:number2' do
-    @number1 = params[:number1].to_i
-    @number2 = params[:number2].to_i
+    @num1 = params[:number1].to_i
+    @num2 = params[:number2].to_i
     @op = params[:operation]
     if @op == "add"
-      #{
+      "#{@num1 + @num2}"
   end
 end
